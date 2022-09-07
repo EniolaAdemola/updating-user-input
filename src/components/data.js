339df1d53e30384f3a63a99291504108577cca83
Eniola Ademola <it.eniolaademola@gmail.com>
2022-09-07 11:51:42 +0100

    add data file

---
+++ src/components/data.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { IoLogoTwitter } from "react-icons/io";
+import { BsGithub } from "react-icons/bs";
+
+export const footerSocials = [
+	{
+		id: 1,
+		link: "https://twitter.com/_daveworld",
+		icon: <IoLogoTwitter />,
+	},
+	{
+		id: 2,
+		link: "https://github.com/EniolaAdemola/updating-user-input",
+		icon: <BsGithub />,
+	},
+];
